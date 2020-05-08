e79ae6b84e5e642ab6553ccde01d0a0dabdf1a5f
DELL <DELL@DESKTOP-HLH6HUP>
2020-05-08 05:00:12 +0300

    #3 - About Us

--- BasduvarMakina/App_Start/RouteConfig.cs
+++ BasduvarMakina/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace BasduvarMakina
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "About",
+                url: "hakkimizda",
+                defaults: new { controller = "About", action = "Index"}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
