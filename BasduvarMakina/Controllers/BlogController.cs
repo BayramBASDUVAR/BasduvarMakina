0713653b445495c8a370fca8d97e83eceeb43d7e
DELL <DELL@DESKTOP-HLH6HUP>
2020-05-08 05:28:54 +0300

    #4 - Blog page

---
+++ BasduvarMakina/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasduvarMakina.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Başduvar | Blog";
+            return View();
+        }
+    }
+}
